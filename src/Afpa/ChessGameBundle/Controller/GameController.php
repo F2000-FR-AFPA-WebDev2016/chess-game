2375fced579fc76247b6563510de00d1f3e45282
cc13090 <christian.chabert@gmail.com>
2016-05-27 11:39:36 +0200

    Create board view

--- src/Afpa/ChessGameBundle/Controller/GameController.php
+++ src/Afpa/ChessGameBundle/Controller/GameController.php
@@ -6,38 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class GameController extends Controller
-{
+class GameController extends Controller {
+
+    protected $theme = 'default';
+
     /**
      * @Route("/home")
      * @Template()
      */
-    public function homeAction()
-    {
+    public function homeAction() {
+        $aPlateau = array();
+        return $this->render('AfpaChessGameBundle:Game:home.html.twig', array(
+                    'theme' => $this->theme,
+                    'board' => $aPlateau
+        ));
     }
 
     /**
      * @Route("/credits")
      * @Template()
      */
-    public function creditsAction()
-    {
+    public function creditsAction() {
+
     }
 
     /**
      * @Route("/rules")
      * @Template()
      */
-    public function rulesAction()
-    {
+    public function rulesAction() {
+
     }
 
     /**
      * @Route("/end")
      * @Template()
      */
-    public function endAction()
-    {
+    public function endAction() {
+
     }
 
 }
