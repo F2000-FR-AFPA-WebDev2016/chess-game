4265ade3d33d43f09d6bbaf860df457daa6f5d97
frelena13 <frelena@gmail.com>
2016-06-01 09:51:47 +0200

    knight

--- src/Afpa/ChessGameBundle/Model/Knight.php
+++ src/Afpa/ChessGameBundle/Model/Knight.php
@@ -5,7 +5,16 @@ namespace Afpa\ChessGameBundle\Model;
 class Knight extends Piece {
 
     public function getMovePossibilities($xInit, $yInit) {
-
+        $aTab = array();
+        $aTab[] = array($xInit + 1, $yInit + 2);
+        $aTab[] = array($xInit + 1, $yInit - 2);
+        $aTab[] = array($xInit + 2, $yInit + 1);
+        $aTab[] = array($xInit + 2, $yInit - 1);
+        $aTab[] = array($xInit - 1, $yInit + 2);
+        $aTab[] = array($xInit - 1, $yInit - 2);
+        $aTab[] = array($xInit - 2, $yInit + 1);
+        $aTab[] = array($xInit - 2, $yInit - 1);
+        return $aTab;
     }
 
 }
