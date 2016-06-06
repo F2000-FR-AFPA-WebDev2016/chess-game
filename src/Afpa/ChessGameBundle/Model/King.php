81b9314e9fefe11176c08fa6f59c91a814a62d44
Alburak <omarmechouahi@gmail.com>
2016-06-06 08:37:58 +0200

    reglage bug king

--- src/Afpa/ChessGameBundle/Model/King.php
+++ src/Afpa/ChessGameBundle/Model/King.php
@@ -5,31 +5,16 @@ namespace Afpa\ChessGameBundle\Model;
 class King extends Piece {
 
     public function getMovePossibilities($xInit, $yInit) {
-        $aMoves = array();
-
-        $aDir = array(array(1, 0), array(0, 1), array(-1, 0), array(0, -1), array(-1, -1), array(-1, 1), array(1, 1), array(1, -1));
-        $i = 0;
-        foreach ($aDir as $aAxe) {
-            $x = $xInit;
-            $y = $yInit;
-
-            do {
-                $x += $aAxe[0];
-                $y += $aAxe[1];
-
-                $bOk = Chessboard::isCoordsValid($x, $y);
-                if ($bOk) {
-                    if (!isset($aMoves[$i])) {
-                        $aMoves[$i] = array();
-                    }
-                    $aMoves[$i][] = array($x, $y);
-                }
-            } while ($bOk);
-
-            $i++;
-        }
-
-        return $aMoves;
+        $aTab = array();
+        $aTab[] = array($xInit + 1, $yInit);
+        $aTab[] = array($xInit, $yInit + 1);
+        $aTab[] = array($xInit - 1, $yInit);
+        $aTab[] = array($xInit, $yInit - 1);
+        $aTab[] = array($xInit - 1, $yInit - 1);
+        $aTab[] = array($xInit - 1, $yInit + 1);
+        $aTab[] = array($xInit + 1, $yInit + 1);
+        $aTab[] = array($xInit + 1, $yInit - 1);
+        return $aTab;
     }
 
 }
